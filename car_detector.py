f0f937390f8cd31cc85ab4a82d4e136c9167e687
Denys Lazarenko <denis.lazarenko@gmail.com>
2025-04-20 14:37:13 +0300

    Switch to YOLOv8n, use ultralytics pip package

--- car_detector.py
+++ car_detector.py
@@ -1,5 +1,5 @@
 import cv2
-import torch
+from ultralytics import YOLO
 
 
 CAR_CLASS_ID = 2  # (COCO class 2 is 'car')
@@ -8,10 +8,9 @@ CAR_CLASS_ID = 2  # (COCO class 2 is 'car')
 class CarDetector:
     def __init__(self):
         """
-        Load a Yolov5 model for car detection
-        TODO Load the model from the local file without downloading again
+        Load a Yolov5 model for car detection.
         """
-        self.model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5s.pt', force_reload=False)
+        self.model = YOLO("yolov8n.pt")  # or "yolov8s.pt" for a slightly better accuracy TODO benchmark with some metric
         print(self.model)
 
     def detect_cars(self, frame):
@@ -19,15 +18,14 @@ class CarDetector:
         Run YOLO inference to detect cars only.
         """
         results = self.model(frame)
-        detections = results.xyxy[0]  # x1, y1, x2, y2, conf, class
+        boxes = results[0].boxes
+
         car_boxes = []
 
-        for *box, conf, cls in detections:
-            if int(cls) == CAR_CLASS_ID:
-                x1, y1, x2, y2 = map(int, box)
-                car_boxes.append((x1, y1, x2, y2, conf.item()))
-
-                # Draw bounding box and label on frame
+        for xyxy, conf, cls in zip(boxes.xyxy, boxes.conf, boxes.cls):
+            if int(cls.item()) == CAR_CLASS_ID:
+                x1, y1, x2, y2 = map(int, xyxy.tolist())
+                car_boxes.append((x1, y1, x2, y2, float(conf.item())))
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
                 cv2.putText(frame, f"Car {conf:.2f}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
